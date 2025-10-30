c795a42fbe5509407854caee91e2229f8f2da6c6
HessaAlhuno <159720557+HessaAlhuno@users.noreply.github.com>
2025-10-30 05:17:41 +0300

    Add WaitingApproved screen for user feedback

---
+++ LawMateFeatures/Sprint1_Authentication/waitingApproved.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class WaitingApproved extends StatelessWidget {
+  const WaitingApproved({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF4F4F4),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/Beige_and_Brown_Aesthetic_Background_Instagram_Story2.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back_ios_new, size: 24),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/signUpUserLawyer');
+                },
+              ),
+            ),
+            const Spacer(flex: 2),
+            Center(
+              child: Text(
+                '! تم أستلام طلبك',
+                style: const TextStyle(
+                  fontFamily: 'Inter Tight',
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'شكرًا لتسجيلك! سيتم مراجعة طلبك خلال 72 ساعة. في حال الموافقة، ستتمكن من تسجيل الدخول. نقدر صبرك وتفهمك.',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: 'Inter Tight',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const Spacer(flex: 3),
+          ],
+        ),
+      ),
+    );
+  }
+}
